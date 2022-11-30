0172ce18f031ad55c68de47078c492507e036f05
Nathaniel Schaaf <schana@noreply.users.github.com>
2022-11-29 19:35:38 -0600

    Add CDK template

---
+++ tests/unit/test_skillsusa_python_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from skillsusa_python.skillsusa_python_stack import SkillsusaPythonStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in skillsusa_python/skillsusa_python_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = SkillsusaPythonStack(app, "skillsusa-python")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
