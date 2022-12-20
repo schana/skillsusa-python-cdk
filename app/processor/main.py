7ac70192729402933c04ee3971df3c728fd93334
Nathaniel Schaaf <schana@users.noreply.github.com>
2022-12-20 14:54:01 -0600

    Add step function state machine submission processing

--- app/processor/main.py
+++ app/processor/main.py
@@ -1,6 +1,39 @@
-import json
+import typing
+
+import boto3
+import os
+from aws_lambda_powertools.utilities.typing import LambdaContext
+from aws_lambda_powertools.utilities.data_classes import event_source, SQSEvent
+
+if typing.TYPE_CHECKING:
+    from mypy_boto3_s3.service_resource import Bucket, BucketObjectsCollection
+    from mypy_boto3_s3 import S3ServiceResource
+    from mypy_boto3_stepfunctions import SFNClient
+else:
+    S3Client = object
+    S3ServiceResource = object
+    Bucket = object
+    SFNClient = object
 
 
-def main(event, context):
-    print(json.dumps(event))
-    print(json.dumps(context))
+@event_source(data_class=SQSEvent)
+def start_processing(event: SQSEvent, context: LambdaContext):
+    sfn: SFNClient = boto3.client("stepfunctions")
+    sfn.start_execution(stateMachineArn=os.environ["STATE_MACHINE_ARN"])
+
+
+def process(event, context):
+    print(event)
+    print(context)
+
+
+def pre_process(event: dict, context: LambdaContext):
+    s3: S3ServiceResource = boto3.resource("s3")
+    bucket: Bucket = s3.Bucket(event.get("bucket"))
+    objects: BucketObjectsCollection = bucket.objects.filter(Prefix="private/")
+    for s3_object in objects:
+        print(s3_object.key)
+
+
+def validate(event, context):
+    pass
--- sneks/sneks_stack.py
+++ sneks/sneks_stack.py
@@ -8,6 +8,10 @@ from aws_cdk import (
     aws_lambda_python_alpha as lambda_python,
     aws_events as events,
     aws_events_targets as targets,
+    aws_sqs as sqs,
+    aws_stepfunctions as step_functions,
+    aws_stepfunctions_tasks as tasks,
+    Duration,
 )
 from constructs import Construct
 
@@ -44,17 +48,34 @@ class SneksStack(Stack):
             auto_delete_objects=True,
         )
 
-        processor = lambda_python.PythonFunction(
-            self,
-            "Processor",
-            runtime=lambda_.Runtime.PYTHON_3_9,
-            entry="./app/processor",
-            index="main.py",
-            handler="main",
+        start_processor = self.build_python_lambda("StartProcessor", "start_processing")
+        pre_processor = self.build_python_lambda(
+            name="PreProcessor", handler="pre_process"
         )
+        validator = self.build_python_lambda(name="Validator", handler="validate")
+        processor = self.build_python_lambda(name="Processor", handler="process")
 
+        submission_bucket.grant_read_write(pre_processor)
+        submission_bucket.grant_read(validator)
         submission_bucket.grant_read(processor)
         results_bucket.grant_read_write(processor)
+
+        dead_letter_queue = sqs.Queue(
+            self,
+            "DeadLetterQueue",
+            fifo=True,
+            retention_period=Duration.days(14),
+        )
+
+        submission_queue = sqs.Queue(
+            self,
+            "SubmissionQueue",
+            fifo=True,
+            content_based_deduplication=True,
+            dead_letter_queue=sqs.DeadLetterQueue(
+                max_receive_count=3, queue=dead_letter_queue
+            ),
+        )
 
         rule = events.Rule(
             self,
@@ -67,14 +88,46 @@ class SneksStack(Stack):
             ),
         )
 
-        rule.add_target(target=targets.LambdaFunction(processor))
-
-        submission_event = lambda_event_sources.S3EventSource(
-            bucket=submission_bucket,
-            events=[s3.EventType(s3.EventType.OBJECT_CREATED_PUT)],
+        # Use SQS to provide deduplication so our downstream workflow only runs at most once every 5 minutes
+        rule.add_target(
+            target=targets.SqsQueue(
+                queue=submission_queue,
+                message=events.RuleTargetInput.from_event_path("$.detail.bucket.name"),
+                message_group_id="submission",
+            )
         )
 
-        processor.add_event_source(submission_event)
+        task_wait = step_functions.Wait(
+            self,
+            "WaitForUploadComplete",
+            time=step_functions.WaitTime.duration(
+                Duration.minutes(1)  # TODO: replace minutes with 5
+            ),
+        )
+
+        task_pre_process = tasks.LambdaInvoke(
+            self,
+            "PreProcessTask",
+            lambda_function=pre_processor,
+            payload=step_functions.TaskInput.from_object(
+                dict(bucket=submission_bucket.bucket_name)
+            ),
+        )
+
+        definition = task_wait.next(task_pre_process)
+
+        workflow = step_functions.StateMachine(
+            self, "Workflow", definition=definition, timeout=Duration.minutes(5)
+        )
+        start_processor.add_environment("STATE_MACHINE_ARN", workflow.state_machine_arn)
+        workflow.grant_start_execution(start_processor)
+
+        # Trigger the workflow from SQS by using a lambda
+        submission_event = lambda_event_sources.SqsEventSource(
+            queue=submission_queue,
+            batch_size=1,
+        )
+        start_processor.add_event_source(submission_event)
 
         static_site = StaticSite(
             self, "StaticSite", submission_bucket=submission_bucket
@@ -85,4 +138,14 @@ class SneksStack(Stack):
             "SneksSubmissionBucket",
             value=submission_bucket.bucket_name,
             export_name="SneksSubmissionBucket",
+        )
+
+    def build_python_lambda(self, name: str, handler: str):
+        return lambda_python.PythonFunction(
+            self,
+            name,
+            runtime=lambda_.Runtime.PYTHON_3_9,
+            entry="./app/processor",
+            index="main.py",
+            handler=handler,
         )
