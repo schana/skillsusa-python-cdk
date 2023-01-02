e0812cb57e2c1db6cab72d77b172c4cd32cecffe
Nathaniel Schaaf <schana@users.noreply.github.com>
2023-01-01 21:40:09 -0600

    Deploy stack again

--- app/sneks/src/aws-config.js
+++ app/sneks/src/aws-config.js
@@ -1,14 +1,14 @@
 export const config = {
   Auth: {
     region: "us-east-1",
-    userPoolId: "us-east-1_4EEfF9eA7",
-    userPoolWebClientId: "5qm6a3o6hmkjirpqnqkgmu0o8v",
-    identityPoolId: "us-east-1:802cdf5a-68fd-41d2-8924-153e6e677a91",
+    userPoolId: "us-east-1_8EnjMe66B",
+    userPoolWebClientId: "7r3rvvcvv4qh8jq54lcm5lr62q",
+    identityPoolId: "us-east-1:b6a8e1a0-2295-4f0a-af26-ad6e7d78d215",
     mandatorySignIn: true,
   },
   Storage: {
     AWSS3: {
-      bucket: "sneksstack-submissionbucket0a5b2457-udlx0b7r7fbb",
+      bucket: "sneksstack-submissionbucket0a5b2457-e9uirbjfl3mr",
       region: "us-east-1",
     },
   },
