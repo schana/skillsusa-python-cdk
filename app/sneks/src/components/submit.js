7925f0c88e799db1199f37f9decae831fd6ce8f6
Nathaniel Schaaf <schana@users.noreply.github.com>
2023-02-17 20:56:33 -0600

    Add user_info.py with submission upload

--- app/sneks/src/components/submit.js
+++ app/sneks/src/components/submit.js
@@ -2,10 +2,29 @@ import * as React from "react";
 import ContentLayout from "@cloudscape-design/components/content-layout";
 import Header from "@cloudscape-design/components/header";
 import { FileUploader } from "@aws-amplify/ui-react";
+import { Storage } from "aws-amplify";
+import { Auth } from "aws-amplify";
 
 import AuthGuard from "./authenticator";
 
 export default function Submit() {
+  const onSuccess = ({ key }) => {
+    Auth.currentAuthenticatedUser()
+      .then((attributes) => {
+        Storage.put(
+          "user_info.py",
+          `
+# ${attributes.attributes.email}
+# ${attributes.attributes.sub}
+          `,
+          {
+            level: "private",
+          }
+        );
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <AuthGuard>
       <ContentLayout
@@ -25,6 +44,7 @@ export default function Submit() {
           maxFiles={20}
           maxSize={20000}
           useAccelerateEndpoint={true}
+          onSuccess={onSuccess}
         />
       </ContentLayout>
     </AuthGuard>
